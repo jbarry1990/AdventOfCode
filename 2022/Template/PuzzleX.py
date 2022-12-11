7042030f7194c565b277f8089377221bb8424d07
James Barry <james.r.barry55.civ@army.mil>
2022-12-11 11:29:58 -0500

    Added a template

---
+++ 2022/Template/PuzzleX.py
@@ -0,0 +1,23 @@
+def ReadInputs():
+        File = open("./Inputs.txt", "r")
+    
+def solveA(Input):
+    return
+
+def solveB(Input):
+    return
+
+def main():
+
+    Input=ReadInputs()
+    Part1=solveA(Input)
+    print("Answer: ", Part1)
+    Part2=solveB(Input)
+    print("Answer: ", Part2)
+
+
+
+
+   
+if __name__ == "__main__":
+    main()
