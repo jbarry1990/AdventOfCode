de2aecb514eed2dfa7ddba7a6c8e75be63568b4b
James Barry <james.r.barry55.civ@army.mil>
2022-12-13 13:55:28 -0500

    Working on day 9 part 1

--- 2022/Day 9/Puzzle9.py
+++ 2022/Day 9/Puzzle9.py
@@ -1,7 +1,49 @@
 def ReadInputs():
         File = open("./Inputs.txt", "r")
-    
-def solveA(Input):
+
+        Steps = [Line.strip().split() for Line in File]
+
+        for Index,Step in enumerate(Steps):
+            Steps[Index][1] = int(Step[1])
+
+        return Steps
+
+def solveA(Steps):
+
+    Visited = {}
+    HeadCoordinates = [0,0]
+    TailCoordinates = [0,0]
+
+
+    for Step in Steps:
+        if Step[0] == "R":
+            for i in range(Step[1]):
+                HeadCoordinates[0] +=1
+                if HeadCoordinates[1] == TailCoordinates[1]:
+                    if abs(HeadCoordinates[0] - TailCoordinates[0]) == 2:
+                        TailCoordinates[0] +=1
+                        if TailCoordinates in Visited:
+                            Visited[TailCoordinates] +=1
+                        else:
+                            Visited[TailCoordinates] = 1
+                else:
+                    
+
+
+
+        elif Step[0] == "L":
+            for i in range(Step[1]):
+                HeadCoordinates[0] -=1
+
+        elif Step[0] == "U":
+            for i in range(Step[1]):
+                HeadCoordinates[1] +=1
+
+        elif Step[0] == "D":
+            for i in range(Step[1]):
+                HeadCoordinates[1] -=1
+
+    print(HeadCoordinates)
     return
 
 def solveB(Input):
