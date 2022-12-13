18a4234e41187f45eb2fbd466799031aee93ee09
James Barry <jbarry@dcscorp.com>
2022-12-13 17:05:39 -0500

    Finished day 9 part 1

--- 2022/Day 9/Puzzle9.py
+++ 2022/Day 9/Puzzle9.py
@@ -10,41 +10,102 @@ def ReadInputs():
 
 def solveA(Steps):
 
-    Visited = {}
-    HeadCoordinates = [0,0]
-    TailCoordinates = [0,0]
+    Visited = set()
+    Head = [0,0]
+    Tail = [0,0]
+    X = 0
+    Y = 1
+    Visited.add(str(Tail[X])+","+str(Tail[Y]))
 
 
+    Count = 0
     for Step in Steps:
+        Count +=1
         if Step[0] == "R":
             for i in range(Step[1]):
-                HeadCoordinates[0] +=1
-                if HeadCoordinates[1] == TailCoordinates[1]:
-                    if abs(HeadCoordinates[0] - TailCoordinates[0]) == 2:
-                        TailCoordinates[0] +=1
-                        if TailCoordinates in Visited:
-                            Visited[TailCoordinates] +=1
-                        else:
-                            Visited[TailCoordinates] = 1
-                else:
-                    
-
-
-
+                Head[X] +=1
+                if Head[Y] == Tail[Y]:
+                    if abs(Head[X] - Tail[X]) == 2:
+                        Tail[X] +=1
+                        IndexString =  str(Tail[X]) + "," + str(Tail[Y])
+                        Visited.add(IndexString)
+                            
+                elif Tail[X]+2 == Head[X] and Tail[Y]+1 == Head[Y]:
+                    Tail[X] +=1
+                    Tail[Y] +=1
+                    IndexString =  str(Tail[X]) + "," + str(Tail[Y])
+                    Visited.add(IndexString)
+                                    
+                elif Tail[X]+2 == Head[X] and Tail[Y]-1 == Head[Y]:
+                    Tail[X] +=1
+                    Tail[Y] -=1
+                    IndexString =  str(Tail[X]) + "," + str(Tail[Y])
+                    Visited.add(IndexString)
+                                    
         elif Step[0] == "L":
             for i in range(Step[1]):
-                HeadCoordinates[0] -=1
-
+                Head[X] -=1
+                if Head[Y] == Tail[Y]:
+                    if abs(Head[X] - Tail[X]) == 2:
+                        Tail[X] -=1
+                        IndexString =  str(Tail[X]) + "," + str(Tail[Y])
+                        Visited.add(IndexString)
+                            
+                elif Tail[X]-2 == Head[X] and Tail[Y]+1 == Head[Y]:
+                    Tail[X] -=1
+                    Tail[Y] +=1
+                    IndexString =  str(Tail[X]) + "," + str(Tail[Y])
+                    Visited.add(IndexString)
+                            
+                elif Tail[X]-2 == Head[X] and Tail[Y]-1 == Head[Y]:
+                    Tail[X] -=1
+                    Tail[Y] -=1
+                    IndexString =  str(Tail[X]) + "," + str(Tail[Y])
+                    Visited.add(IndexString)
+                    
         elif Step[0] == "U":
             for i in range(Step[1]):
-                HeadCoordinates[1] +=1
+                Head[Y] +=1
+                if Head[X] == Tail[X]:
+                    if abs(Head[Y] - Tail[Y]) == 2:
+                        Tail[Y] +=1
+                        IndexString =  str(Tail[X]) + "," + str(Tail[Y])
+                        Visited.add(IndexString)
+                            
+                elif Tail[Y]+2 == Head[Y] and Tail[X]+1 == Head[X]:
+                    Tail[Y] +=1
+                    Tail[X] +=1
+                    IndexString =  str(Tail[X]) + "," + str(Tail[Y])
+                    Visited.add(IndexString)
+                                    
+                elif Tail[Y]+2 == Head[Y] and Tail[X]-1 == Head[X]:
+                    Tail[Y] +=1
+                    Tail[X] -=1
+                    IndexString =  str(Tail[X]) + "," + str(Tail[Y])
+                    Visited.add(IndexString)
 
         elif Step[0] == "D":
             for i in range(Step[1]):
-                HeadCoordinates[1] -=1
+                Head[Y] -=1
+                if Head[X] == Tail[X]:
+                    if abs(Head[Y] - Tail[Y]) == 2:
+                        Tail[Y] -=1
+                        IndexString =  str(Tail[X]) + "," + str(Tail[Y])
+                        Visited.add(IndexString)
+                            
+                elif Tail[Y]-2 == Head[Y] and Tail[X]+1 == Head[X]:
+                    Tail[Y] -=1
+                    Tail[X] +=1
+                    IndexString =  str(Tail[X]) + "," + str(Tail[Y])
+                    Visited.add(IndexString)
+                                    
+                elif Tail[Y]-2 == Head[Y] and Tail[X]-1 == Head[X]:
+                    Tail[Y] -=1
+                    Tail[X] -=1
+                    IndexString =  str(Tail[X]) + "," + str(Tail[Y])
+                    Visited.add(IndexString)
 
-    print(HeadCoordinates)
-    return
+    return len(Visited)
 
 def solveB(Input):
     return
