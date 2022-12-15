5af0d79b799224afabb293b5015dc9337e7dd3e4
James Barry <james.r.barry55.civ@army.mil>
2022-12-15 08:44:39 -0500

    Files for day 11

---
+++ 2022/Day 11/Puzzle11.py
@@ -0,0 +1,32 @@
+def ReadInputs():
+        File = open("./Inputs.txt", "r")
+
+        Input = [Line.strip() for Line in File]
+        
+        LineCounter = 0
+        StartingItems = {}
+        Operations = []
+        Divisible = []
+        Whereto = []
+        for Data in Input:
+    
+def solveA(Input):
+    return
+
+def solveB(Input):
+    return
+
+def main():
+
+    Input=ReadInputs()
+    Part1=solveA(Input)
+    print("Answer: ", Part1)
+    Part2=solveB(Input)
+    print("Answer: ", Part2)
+
+
+
+
+   
+if __name__ == "__main__":
+    main()
