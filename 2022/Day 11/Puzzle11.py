f2ccf277513070e6f45848c43939e7242bbbe4b4
James Barry <jbarry@dcscorp.com>
2022-12-15 14:17:34 -0500

    Finished day 11

--- 2022/Day 11/Puzzle11.py
+++ 2022/Day 11/Puzzle11.py
@@ -1,27 +1,70 @@
+import math
+
 def ReadInputs():
-        File = open("./Inputs.txt", "r")
+    File = open("./Inputs.txt", "r")
 
-        Input = [Line.strip() for Line in File]
-        
-        LineCounter = 0
-        StartingItems = {}
-        Operations = []
-        Divisible = []
-        Whereto = []
-        for Data in Input:
-    
-def solveA(Input):
-    return
+    Input = File.read().strip()
 
-def solveB(Input):
-    return
+    StartingItems = []
+    Operations = []
+    Divisible = []
+    Whereto = []
+
+    for Monkey in Input.split("\n\n"):
+        ID, Items, Operation, Test, IfTrue, IfFalse = Monkey.split("\n")
+        StartingItems.append([int(i) for i in Items.split(":")[1].split(",")])
+        Words = Operation.split()
+        Operations.append("".join(Words[-3:]))
+        Divisible.append(int(Test.split()[-1]))
+        Whereto.append([int(IfTrue.split()[-1]), int(IfFalse.split()[-1])])
+
+    return StartingItems, Operations, Divisible, Whereto
+
+def solveA(StartingItems, Operations, Divisible, Whereto):
+    Count  = [0 for _ in range(len(StartingItems))]
+    for _ in range(20):
+        for i in range(len(StartingItems)):
+            for Item in StartingItems[i]:
+                Count[i] +=1
+                old = Item
+                Item = eval(Operations[i])
+                Item = Item//3
+                if Item % Divisible[i] == 0:
+                    StartingItems[Whereto[i][0]].append(Item)
+                else:
+                    StartingItems[Whereto[i][1]].append(Item)
+            StartingItems[i] = []
+
+    return sorted(Count)[-1]*sorted(Count)[-2]
+
+def solveB(StartingItems, Operations, Divisible, Whereto):
+    LCM = 1
+    for x in Divisible:
+        LCM *= (LCM*x)
+
+    Count  = [0 for _ in range(len(StartingItems))]
+    for _ in range(10000):
+        for i in range(len(StartingItems)):
+            for Item in StartingItems[i]:
+                Count[i] +=1
+                old = Item
+                Item = eval(Operations[i])
+                Item = Item%LCM
+                if Item % Divisible[i] == 0:
+                    StartingItems[Whereto[i][0]].append(Item)
+                else:
+                    StartingItems[Whereto[i][1]].append(Item)
+            StartingItems[i] = []
+
+    return sorted(Count)[-1]*sorted(Count)[-2]
 
 def main():
 
-    Input=ReadInputs()
-    Part1=solveA(Input)
+    StartingItems, Operations, Divisible, Whereto=ReadInputs()
+    Part1=solveA(StartingItems, Operations, Divisible, Whereto)
     print("Answer: ", Part1)
-    Part2=solveB(Input)
+    StartingItems, Operations, Divisible, Whereto=ReadInputs()
+    Part2=solveB(StartingItems, Operations, Divisible, Whereto)
     print("Answer: ", Part2)
 
 
