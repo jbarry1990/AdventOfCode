81d4aa90aac6f469fd1d1a6ab3dd255ad71b2a2d
James Barry <jbarry@dcscorp.com>
2022-12-21 23:02:07 -0500

    Finished day 19

--- 2022/Day 19/Puzzle19.py
+++ 2022/Day 19/Puzzle19.py
@@ -1,3 +1,4 @@
+from collections import deque
 def ReadInputs():
         File = open("./Inputs.txt", "r")
         Data = File.read().strip()
@@ -7,34 +8,58 @@ def ReadInputs():
 
 def CollectGeode(OreBotCost, ClayBotCost, ObsidianBotCost, GeodeBotCost, StartTime, Resources, BotCounts):
     best = 0
-    Queue = []
-    State = (Resources[0],Resources[1], Resources[2], Resources[3], BotCounts[0], BotCounts[1], BotCounts[2], BotCounts[3], StartTime)
-    Queue.append(State)
+    MaxOreCost = max(OreBotCost[0], ClayBotCost[0], ObsidianBotCost[0], GeodeBotCost[0])
+    MaxClayCost = ObsidianBotCost[1]
+    MaxObsidianCost = GeodeBotCost[1]
+    MaxOreCost = max(OreBotCost[0], ClayBotCost[0], ObsidianBotCost[0], GeodeBotCost[0])
+    State = (0,0,0,0,1,0,0,0, StartTime)
+    Queue = deque([State])
     print(Queue)
     Seen = set()
-    while Queue != []:
-        State = Queue.pop()
+    #for _ in range(3):
+    while Queue:
+        State = Queue.popleft()
 
+        OreCount, ClayCount, ObsidianCount, GeodeCount, OreBotCount, ClayBotCount, ObsidianBotCount, GeodeBotCount, Time = State
+
+        #print(GeodeCount, ": ", best)
+        best = max(best, GeodeCount)
+        if Time == 0:
+            continue
+        if OreBotCount >= MaxOreCost:
+            OreBotCount = MaxOreCost
+        if ClayBotCount >= MaxClayCost:
+            ClayBotCount = MaxClayCost
+        if ObsidianBotCount >= MaxObsidianCost:
+            ObsidianBotCount = MaxObsidianCost
+        if OreCount >= Time*MaxOreCost-OreBotCount*(Time-1):
+            OreCount = Time*MaxOreCost-OreBotCount*(Time-1)
+        if ClayCount>=Time*ClayBotCost[1]-ClayBotCount*(Time-1):
+            ClayCount = Time*ClayBotCost[1]-ClayBotCount*(Time-1)
+        if ObsidianCount>=Time*GeodeBotCost[1]-ObsidianBotCount*(Time-1):
+            ObsidianCount = Time*GeodeBotCost[1]-ObsidianBotCount*(Time-1)
+
+        state = (OreCount,ClayCount,ObsidianCount,GeodeCount,OreBotCount,ClayBotCount,ObsidianBotCount,GeodeBotCount,Time)
         if State in Seen:
             continue
         Seen.add(State)
 
-        OreCount, ClayCount, ObsidianCount, GeodeCount, OreBotCount, ClayBotCount, ObsidianBotCount, GeodeBotCount, Time = State
-
-        best = max(best, GeodeCount)
-        if Time == 0:
-            continue
-        
         # Collect resources
-        Queue.insert(0,(OreCount+OreBotCount, ClayCount+ClayBotCount, ObsidianCount+ObsidianBotCount, GeodeCount+GeodeBotCount, OreBotCount, ClayBotCount, ObsidianBotCount, GeodeBotCount, Time-1))
-        if OreCount >= OreBotCost[0]:
-            Queue.insert(0,(OreCount+OreBotCount-OreBotCost[0], ClayCount+ClayBotCount, ObsidianCount+ObsidianBotCount, GeodeCount+GeodeBotCount, OreBotCount+1, ClayBotCount, ObsidianBotCount, GeodeBotCount, Time-1))
-        if ClayCount >= ClayBotCost[0]:
-            Queue.insert(0,(OreCount+OreBotCount-ClayBotCost[0], ClayCount+ClayBotCount, ObsidianCount+ObsidianBotCount, GeodeCount+GeodeBotCount, OreBotCount, ClayBotCount+1, ObsidianBotCount, GeodeBotCount, Time-1))
-        if OreCount >= ObsidianBotCost[0] and ClayCount >= ObsidianBotCost[1]:
-            Queue.insert(0,(OreCount+OreBotCount-ObsidianBotCost[0], ClayCount+ClayBotCount-ObsidianBotCost[1], ObsidianCount+ObsidianBotCount, GeodeCount+GeodeBotCount, OreBotCount, ClayBotCount, ObsidianBotCount+1, GeodeBotCount, Time-1))
+        if len(Seen) % 1000000 == 0:
+            print(best,",",len(Seen))
+        Queue.append((OreCount+OreBotCount, ClayCount+ClayBotCount, ObsidianCount+ObsidianBotCount, GeodeCount+GeodeBotCount, OreBotCount, ClayBotCount, ObsidianBotCount, GeodeBotCount, Time-1))
+        if OreCount >= OreBotCost[0] and OreBotCount:
+            print("I'm buying an OreBot")
+            Queue.append((OreCount+OreBotCount-OreBotCost[0], ClayCount+ClayBotCount, ObsidianCount+ObsidianBotCount, GeodeCount+GeodeBotCount, OreBotCount+1, ClayBotCount, ObsidianBotCount, GeodeBotCount, Time-1))
+        if OreCount >= ClayBotCost[0] and ClayBotCount:
+            print("I'm buying a ClayBot")
+            Queue.append((OreCount+OreBotCount-ClayBotCost[0], ClayCount+ClayBotCount, ObsidianCount+ObsidianBotCount, GeodeCount+GeodeBotCount, OreBotCount, ClayBotCount+1, ObsidianBotCount, GeodeBotCount, Time-1))
+        if OreCount >= ObsidianBotCost[0] and ClayCount >= ObsidianBotCost[1] and ObsidianBotCount:
+            Queue.append((OreCount+OreBotCount-ObsidianBotCost[0], ClayCount+ClayBotCount-ObsidianBotCost[1], ObsidianCount+ObsidianBotCount, GeodeCount+GeodeBotCount, OreBotCount, ClayBotCount, ObsidianBotCount+1, GeodeBotCount, Time-1))
         if OreCount >= GeodeBotCost[0] and ObsidianCount >= GeodeBotCost[1]:
-            Queue.insert(0,(OreCount+OreBotCount-GeodeBotCost[0], ClayCount+ClayBotCount, ObsidianCount+ObsidianBotCount-GeodeBotCost[1], GeodeCount+GeodeBotCount, OreBotCount, ClayBotCount, ObsidianBotCount, GeodeBotCount+1, Time-1))
+            print("We have a geodebot")
+            Queue.append((OreCount+OreBotCount-GeodeBotCost[0], ClayCount+ClayBotCount, ObsidianCount+ObsidianBotCount-GeodeBotCost[1], GeodeCount+GeodeBotCount, OreBotCount, ClayBotCount, ObsidianBotCount, GeodeBotCount+1, Time-1))
+
     return best
 
 def solveA(Blueprints):
