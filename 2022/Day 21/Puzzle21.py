b9f9ac1156b01b4641846363000f7b45ac465fb3
James Barry <james.r.barry55.civ@army.mil>
2022-12-22 23:11:10 -0500

    Added day 21

--- 2022/Day 21/Puzzle21.py
+++ 2022/Day 21/Puzzle21.py
@@ -1,3 +1,5 @@
+import sympy
+
 def ReadInputs():
         File = open("./Inputs.txt", "r")
         Data = File.read().strip()
@@ -32,13 +34,40 @@ def solveA(Solved, Equations):
     return int(Solved["root"])
 
 def solveB(Solved, Equations):
-    return
+    Answer = 0
+    monkeys = { "humn": sympy.Symbol("x") }
+
+    x = [line.strip() for line in open(0)]
+
+    ops = {
+        "+": lambda x, y: x + y,
+        "-": lambda x, y: x - y,
+        "*": lambda x, y: x * y,
+        "/": lambda x, y: x / y,
+    }
+
+    for a in x:
+        name, expr = a.split(": ")
+        if name in monkeys: continue
+        if expr.isdigit():
+            monkeys[name] = sympy.Integer(expr)
+        else:
+            left, op, right = expr.split()
+            if left in monkeys and right in monkeys:
+                if name == "root":
+                    Answer = sympy.solve(monkeys[left] - monkeys[right])[0]
+                    break
+                monkeys[name] = ops[op](monkeys[left], monkeys[right])
+            else:
+                x.append(a)
+    return Answer
 
 def main():
 
     Solved, Equations=ReadInputs()
     Part1=solveA(Solved, Equations)
     print("Answer: ", Part1)
+    Solved, Equations=ReadInputs()
     Part2=solveB(Solved,Equations)
     print("Answer: ", Part2)
 
