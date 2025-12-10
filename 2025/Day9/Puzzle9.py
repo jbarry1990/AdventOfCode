70ff45bfb594e44abd2c5e4dd236fe568c2bc60e
James Barry <james.r.barry55.ctr@army.mil>
2025-12-10 13:06:24 -0500

    Updated part 2 to actually work on the puzzle input

--- 2025/Day9/Puzzle9.py
+++ 2025/Day9/Puzzle9.py
@@ -1,3 +1,4 @@
+import shapely
 def ReadInputs():
     File = open("./Inputs.txt", "r")
     results = [line.strip().split(",") for line in File]
@@ -12,105 +13,119 @@ def solveA(Input):
     return max(areas)
                 
 def solveB(Input):
+    polygon = shapely.Polygon(Input)
+    shapely.prepare(polygon)
 
-    edges = []
-    horizontal = []
-    areas = []
-    p1 = Input[0]
 
-    for i in range(1,len(Input)+1):
-        p2 = Input[i%len(Input)]
-        x1,y1 = p1
-        x2,y2 = p2
-        edges.append(p1)
+    ans2 = 0
+    for p1 in Input:
+        for p2 in Input:
+            corners = [p1, (p1[0], p2[1]), p2, (p2[0], p1[1])]
+            area = (abs(p1[0]-p2[0])+1)*(abs(p1[1]-p2[1])+1)
+            rect = shapely.Polygon(corners)
+            if polygon.contains(rect) and area > ans2:
+                ans2 = area
+    return ans2
 
-        if x1-x2 == 0:
-            if y1 < y2:
-                for i in range(1,(y2-y1)):
-                    edges.append([x1,y1+i])
-            else:
-                for i in range(1,(y1-y2)):
-                    edges.append([x1,y1-i])
-        elif y1-y2 == 0:
-            horizontal.append(p1)
-            if x1 < x2:
-                for i in range(1,(x2-x1)):
-                    edges.append([x1+i,y1])
-                    horizontal.append([x1+i,y1])
-            else:
-                for i in range(1,(x1-x2)):
-                    edges.append([x1-i,y1])
-                    horizontal.append([x1-i,y1])
-            horizontal.append(p2)
-        p1=p2
-        
-    shape = edges.copy()
-
-    x_min = min(x for x,y in edges)
-    x_max = max(x for x,y in edges)
-    y_min = min(y for x,y in edges)
-    y_max = max(y for x,y in edges)
-
-    def check(point):
-        [x,y]=point
-        count = 0
-        for xx in range(1,x_max+1):
-            if [x+xx,y] in horizontal:
-                break
-            if [x+xx,y] in edges:
-                count += 1
-        return count
-
-    for x in range(x_min,x_max+1):
-        for y in range(y_min,y_max+1):
-            if [x,y] in edges:
-                continue
-            if check([x,y])%2 != 0:
-                shape.append([x,y])                
-
-    for i,[x1,y1] in enumerate(Input):
-        for [x2,y2] in Input[i+1:]:
-            Valid = True
-            if x1<x2 and y1<y2:
-                for i in range(x1,x2+1):
-                    for j in range(y1,y2+1):
-                        if [i,j] not in shape:
-                            Valid = False
-                            break
-                    if not Valid:
-                        break
-
-            elif x1>x2 and y1>y2:
-                for i in range(x2,x1+1):
-                    for j in range(y2,y1+1):
-                        if [i,j] not in shape:
-                            Valid = False
-                            break
-                    if not Valid:
-                        break
-
-            elif x1<x2 and y1>y2:
-                for i in range(x1,x2+1):
-                    for j in range(y2,y1+1):
-                        if [i,j] not in shape:
-                            Valid = False
-                            break
-                    if not Valid:
-                        break
-                            
-            elif x1>x2 and y1<y2:
-                for i in range(x2,x1+1):
-                    for j in range(y1,y2+1):
-                        if [i,j] not in shape:
-                            Valid = False
-                            break
-                    if not Valid:
-                        break
-
-            if Valid:
-                areas.append((abs(x2-x1)+1)*(abs(y2-y1)+1))
-                
-    return max(areas)
+## This works but not on the scale of the inputs
+##    edges = []
+##    horizontal = []
+##    areas = []
+##    p1 = Input[0]
+##
+##    for i in range(1,len(Input)+1):
+##        p2 = Input[i%len(Input)]
+##        x1,y1 = p1
+##        x2,y2 = p2
+##        edges.append(p1)
+##
+##        if x1-x2 == 0:
+##            if y1 < y2:
+##                for i in range(1,(y2-y1)):
+##                    edges.append([x1,y1+i])
+##            else:
+##                for i in range(1,(y1-y2)):
+##                    edges.append([x1,y1-i])
+##        elif y1-y2 == 0:
+##            horizontal.append(p1)
+##            if x1 < x2:
+##                for i in range(1,(x2-x1)):
+##                    edges.append([x1+i,y1])
+##                    horizontal.append([x1+i,y1])
+##            else:
+##                for i in range(1,(x1-x2)):
+##                    edges.append([x1-i,y1])
+##                    horizontal.append([x1-i,y1])
+##            horizontal.append(p2)
+##        p1=p2
+##        
+##    shape = edges.copy()
+##
+##    x_min = min(x for x,y in edges)
+##    x_max = max(x for x,y in edges)
+##    y_min = min(y for x,y in edges)
+##    y_max = max(y for x,y in edges)
+##
+##    def check(point):
+##        [x,y]=point
+##        count = 0
+##        for xx in range(1,x_max+1):
+##            if [x+xx,y] in horizontal:
+##                break
+##            if [x+xx,y] in edges:
+##                count += 1
+##        return count
+##
+##    for x in range(x_min,x_max+1):
+##        for y in range(y_min,y_max+1):
+##            if [x,y] in edges:
+##                continue
+##            if check([x,y])%2 != 0:
+##                shape.append([x,y])                
+##
+##    for i,[x1,y1] in enumerate(Input):
+##        for [x2,y2] in Input[i+1:]:
+##            Valid = True
+##            if x1<x2 and y1<y2:
+##                for i in range(x1,x2+1):
+##                    for j in range(y1,y2+1):
+##                        if [i,j] not in shape:
+##                            Valid = False
+##                            break
+##                    if not Valid:
+##                        break
+##
+##            elif x1>x2 and y1>y2:
+##                for i in range(x2,x1+1):
+##                    for j in range(y2,y1+1):
+##                        if [i,j] not in shape:
+##                            Valid = False
+##                            break
+##                    if not Valid:
+##                        break
+##
+##            elif x1<x2 and y1>y2:
+##                for i in range(x1,x2+1):
+##                    for j in range(y2,y1+1):
+##                        if [i,j] not in shape:
+##                            Valid = False
+##                            break
+##                    if not Valid:
+##                        break
+##                            
+##            elif x1>x2 and y1<y2:
+##                for i in range(x2,x1+1):
+##                    for j in range(y1,y2+1):
+##                        if [i,j] not in shape:
+##                            Valid = False
+##                            break
+##                    if not Valid:
+##                        break
+##
+##            if Valid:
+##                areas.append((abs(x2-x1)+1)*(abs(y2-y1)+1))
+##                
+##    return max(areas)
 
 def main():
 
